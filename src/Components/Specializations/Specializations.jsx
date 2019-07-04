9c150720c4e324b3ec7a38911bd37f20ece6d7ae
BartoszBartlomiej <bartosz.lewicki@gmail.com>
2019-07-04 18:07:20 +0200

    specializations in progress

--- src/Components/Specializations/Specializations.jsx
+++ src/Components/Specializations/Specializations.jsx
@@ -7,9 +7,41 @@ class Specializations extends Component {
                 <div className='content'>
                     <h3>SPECJALIZACJE</h3>
                     <div className='under_line'/>
-                    <div className='specializations__info'>
-                        <div className="specializations__info__">
+                    <div className='specializations__box'>
+                        <div className="specializations__box__info">
                             <h4>PRAWO PRACY</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae commodi delectus
+                                dignissimos enim fugiat illum molestias nobis repellat, suscipit.</p>
+                        </div>
+                        <div className="specializations__box__info">
+                            <h4>PRAWO GOSPODARCZE</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae commodi delectus
+                                dignissimos enim fugiat illum molestias nobis repellat, suscipit.</p>
+                        </div>
+                        <div className="specializations__box__info">
+                            <h4>PRAWO CYWILNE</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae commodi delectus
+                                dignissimos enim fugiat illum molestias nobis repellat, suscipit.</p>
+                        </div>
+                        <div className="specializations__box__info">
+                            <h4>PRAWO RODZINNE</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae commodi delectus
+                                dignissimos enim fugiat illum molestias nobis repellat, suscipit.</p>
+                        </div>
+                        <div className="specializations__box__info">
+                            <h4>SPADKI</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae commodi delectus
+                                dignissimos enim fugiat illum molestias nobis repellat, suscipit.</p>
+                        </div>
+                        <div className="specializations__box__info">
+                            <h4>PRAWO KARNE</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae commodi delectus
+                                dignissimos enim fugiat illum molestias nobis repellat, suscipit.</p>
+                        </div>
+                        <div className="specializations__box__info">
+                            <h4>PRAWO MEDYCZNE</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae commodi delectus
+                                dignissimos enim fugiat illum molestias nobis repellat, suscipit.</p>
                         </div>
                     </div>
                 </div>
