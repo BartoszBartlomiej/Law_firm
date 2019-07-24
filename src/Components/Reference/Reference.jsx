36c054152672835810e99b7ff1a96ed91be0b2d8
BartoszBartlomiej <bartosz.lewicki@gmail.com>
2019-07-24 16:11:21 +0200

    referecne in progress

--- src/Components/Reference/Reference.jsx
+++ src/Components/Reference/Reference.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Reference extends Component {
     render() {
@@ -6,8 +6,28 @@ class Reference extends Component {
             <div className='reference'>
                 <div className='content'>
                     <h3>REFERENCJE</h3>
+                    <div className='under_line' />
+                    <div className='reference__box'>
+                        <div className='reference__box__info'>
+                            <h4>IntoBeauty</h4>
+                            <h4>Radosław Staważ - prezes</h4>
+                            <div className='under_line' />
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt provident aliquam distinctio neque delectus, reiciendis harum repudiandae beatae pariatur necessitatibus praesentium modi sed, eveniet quod cum commodi dolorum. Tempore, veritatis!</p>
+                        </div>
+                        <div className='reference__box__info'>
+                            <h4>Szymon Piotr Sawicki eloo</h4>
+                            <h4>Szymon Sawicki - właściciel</h4>
+                            <div className='under_line' />
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt provident aliquam distinctio neque delectus, reiciendis harum repudiandae beatae pariatur necessitatibus praesentium modi sed, eveniet quod cum commodi dolorum. Tempore, veritatis!</p>
+                        </div>
+                        {/* <div className='reference__box__info'>
+                            <h4>IntoBeauty</h4>
+                            <h4>Radosław Staważ - prezes</h4>
+                            <div className='under_line' />
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt provident aliquam distinctio neque delectus, reiciendis harum repudiandae beatae pariatur necessitatibus praesentium modi sed, eveniet quod cum commodi dolorum. Tempore, veritatis!</p>
+                        </div> */}
+                    </div>
                 </div>
-
             </div>
         );
     }
--- src/Components/Specializations/Specializations.jsx
+++ src/Components/Specializations/Specializations.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Specializations extends Component {
     render() {
@@ -6,53 +6,53 @@ class Specializations extends Component {
             <div className='specializations'>
                 <div className='content'>
                     <h3>SPECJALIZACJE</h3>
-                    <div className='under_line'/>
+                    <div className='under_line' />
                     <div className='specializations__box'>
                         <div className="specializations__box__info">
                             <h4>PRAWO PRACY</h4>
-                            <div className='under_line'/>
+                            <div className='under_line' />
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae commodi delectus
                                 dignissimos enim fugiat illum molestias nobis repellat, suscipit.</p>
                         </div>
                         <div className="specializations__box__info">
                             <h4>PRAWO GOSPODARCZE</h4>
-                            <div className='under_line'/>
+                            <div className='under_line' />
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae commodi delectus
                                 dignissimos enim fugiat illum molestias nobis repellat, suscipit.</p>
                         </div>
                         <div className="specializations__box__info">
                             <h4>PRAWO AUTORSKIE</h4>
-                            <div className='under_line'/>
+                            <div className='under_line' />
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae commodi delectus
                                 dignissimos enim fugiat illum molestias nobis repellat, suscipit.</p>
                         </div>
                         <div className="specializations__box__info">
                             <h4>PRAWO CYWILNE</h4>
-                            <div className='under_line'/>
+                            <div className='under_line' />
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae commodi delectus
                                 dignissimos enim fugiat illum molestias nobis repellat, suscipit.</p>
                         </div>
                         <div className="specializations__box__info">
                             <h4>PRAWO RODZINNE</h4>
-                            <div className='under_line'/>
+                            <div className='under_line' />
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae commodi delectus
                                 dignissimos enim fugiat illum molestias nobis repellat, suscipit.</p>
                         </div>
                         <div className="specializations__box__info">
                             <h4>SPADKI</h4>
-                            <div className='under_line'/>
+                            <div className='under_line' />
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae commodi delectus
                                 dignissimos enim fugiat illum molestias nobis repellat, suscipit.</p>
                         </div>
                         <div className="specializations__box__info">
                             <h4>PRAWO KARNE</h4>
-                            <div className='under_line'/>
+                            <div className='under_line' />
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae commodi delectus
                                 dignissimos enim fugiat illum molestias nobis repellat, suscipit.</p>
                         </div>
                         <div className="specializations__box__info">
                             <h4>PRAWO MEDYCZNE</h4>
-                            <div className='under_line'/>
+                            <div className='under_line' />
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae commodi delectus
                                 dignissimos enim fugiat illum molestias nobis repellat, suscipit.</p>
                         </div>
