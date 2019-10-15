9a492107ceaf76a49b62e60bc05a9282e5f6076e
BartoszBartlomiej <bartosz.lewicki@gmail.com>
2019-10-15 20:53:00 +0200

    update block component

--- src/Components/Common/Block/index.jsx
+++ src/Components/Common/Block/index.jsx
@@ -6,7 +6,6 @@ const Block = (props) => {
     return (
         <div className="box">
             <h4 className="box__title">{props.title}</h4>
-            <div className='under_line' />
             <p className="box__content">{props.content}</p>
         </div>
     );
