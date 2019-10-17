80bec042ac61f54b849e9391ef4e1f1b094f1084
BartoszBartlomiej <bartosz.lewicki@gmail.com>
2019-10-17 15:59:53 +0200

    added article component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 // import HamburgerMenu from './HamburgerMenu/HamburgerMenu';
 import Icon from './HamburgerMenu/Icon';
 import HamburgerMenu from './HamburgerMenu/HamburgerMenu';
+import Article from '../Common/Article';
 
 
 
@@ -11,39 +12,41 @@ class Header extends Component {
     render() {
         return (
             <header>
-                <div className='content menu__wrapper'>
-                    <div className='law_name'>
-                        <h1>AGATA ROSZKIEWICZ-LEWICKA</h1>
-                        <h2>Kancelaria Adwokacka</h2>
-                    </div>
-                    <nav>
-                        <ul>
-                            <li>
-                                <a href='/'>STRONA GŁÓWNA</a>
-                            </li>
-                            <li>
-                                <a href='/'>O KANCELARII</a>
-                            </li>
-                            <li>
-                                <a href='/'>SPECJALIZACJE</a>
-                            </li>
-                            <li>
-                                <a href='/'>REFERENCJE</a>
-                            </li>
-                            <li>
-                                <a href='/'>CENNIK</a>
-                            </li>
-                            <li>
-                                <a href='/'>KONTAKT</a>
-                            </li>
-                        </ul>
-                    </nav>
+                <Article>
+                    <div className='menu__wrapper'>
+                        <div className='law_name'>
+                            <h1>AGATA ROSZKIEWICZ-LEWICKA</h1>
+                            <h2>Kancelaria Adwokacka</h2>
+                        </div>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a href='/'>STRONA GŁÓWNA</a>
+                                </li>
+                                <li>
+                                    <a href='/'>O KANCELARII</a>
+                                </li>
+                                <li>
+                                    <a href='/'>SPECJALIZACJE</a>
+                                </li>
+                                <li>
+                                    <a href='/'>REFERENCJE</a>
+                                </li>
+                                <li>
+                                    <a href='/'>CENNIK</a>
+                                </li>
+                                <li>
+                                    <a href='/'>KONTAKT</a>
+                                </li>
+                            </ul>
+                        </nav>
 
-                    {/* <HamburgerMenu /> */}
-                    {/* <div className='hamburger'>
+                        {/* <HamburgerMenu /> */}
+                        {/* <div className='hamburger'>
                         <Icon/>
                     </div> */}
-                </div>
+                    </div>
+                </Article>
                 <div className='picture'>
                     <img src='https://adwokatura.krakow.pl/data/domains/1/pl/c_events_calendar/41/2533/images/logoadwokatura.png' />
                 </div>
